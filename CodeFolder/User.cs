58f07272e6c7e9da73d0501677846f9778cd1e0b
Daan1001 <daan1001polet@gmail.com>
2023-10-04 09:17:34 +0200

    Menu begin

---
+++ CodeFolder/User.cs
@@ -0,0 +1,19 @@
+public class User{
+    private String username;
+    private String password;
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    public static void NewUser(){
+        Console.WriteLine("Fill in your username:");
+        String Uname = Console.ReadLine()!;
+        Console.WriteLine("Fill in your password:");
+        String Upassword = Console.ReadLine()!;
+        User user = new User(Uname, Upassword);
+        Console.WriteLine("New user created!");
+        // SomethingWriter writer = new("Accounts.json");
+        // string json = JsonConvert.SerializeObject(selectedSeats);
+        
+    }
+}
