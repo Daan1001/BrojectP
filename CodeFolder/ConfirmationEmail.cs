5518858aa0e154f69766f0c337fefd012aacd195
RedShiftNinja <1064928@hr.nl>
2023-12-18 09:34:13 +0100

    added email notification when flight has been changed

--- CodeFolder/ConfirmationEmail.cs
+++ CodeFolder/ConfirmationEmail.cs
@@ -28,4 +28,27 @@ public class ConfirmationEmail
             client.Disconnect(true);
         }
     }
+
+    public static void SendEditNotification(string username, string emailAddress,string flightCode, string changedSeats)
+    {
+        var message = new MimeMessage();
+        message.From.Add (new MailboxAddress ("Rotterdam Airport", "rotterdamairport.brojectP@outlook.com"));
+        message.To.Add (new MailboxAddress ($"{username}", $"{emailAddress}"));
+        message.Subject = $"Your reservation on flight {flightCode} has been changed.";
+
+        message.Body = new TextPart ("plain") {
+            Text = $"Your reservation on flight {flightCode} has been changed. your new seats are the following: \n{changedSeats}\n\n If do not agree with these changes, please do not hesitate to contact Customer Service."
+        };
+        
+        // Set up the SMTP client for Gmail
+        using (var client = new SmtpClient())
+        {
+            // Connect to the outlook SMTP server
+            client.Connect("smtp.gmail.com", 465, SecureSocketOptions.Auto);
+            //logging into outlook account
+            client.Authenticate("rotterdam.airport.hr@gmail.com", "kfdq itji zevu pxex");
+            client.Send(message);
+            client.Disconnect(true);
+        }
+    }
 }
