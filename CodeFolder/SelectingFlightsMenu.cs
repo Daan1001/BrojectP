2c072c362294f8e1cb4dfcf66ca72f5ea0519b07
Daan1001 <daan1001polet@gmail.com>
2024-01-09 11:12:09 +0100

    all deleted

--- CodeFolder/SelectingFlightsMenu.cs
+++
@@ -1,8 +0,0 @@
-public class SelectingFlights{
-    public static List<Flight> flights = ShowFlights.LoadFlightsFromJson("DataSources/flights.json");
-    private static List<String> options = new List<string>(){ "Search by country", "Search by city", "Show all flights", "<-- Go back" };
-    public static void Start(){
-        OptionSelection.Start(SelectingFlights.options);
-    }
-}
-
